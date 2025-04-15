410e29fb37e69e6f96bdbdd44f9895f048b804a9
J4C62 <J4NTO@proton.me>
2025-04-15 18:03:58 +0200

    Starting project

---
+++ src/main/java/com/github/j4c62/pms/booking/BookingService.java
@@ -0,0 +1,12 @@
+package com.github.j4c62.pms.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingService {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookingService.class, args);
+  }
+}
---
+++ src/test/java/com/github/j4c62/pms/booking/BookingServiceTests.java
@@ -0,0 +1,11 @@
+package com.github.j4c62.pms.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceTests {
+
+  @Test
+  void contextLoads() {}
+}
